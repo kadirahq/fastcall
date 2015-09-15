ba6146e06d64e77dacecbd7444cda21edc48e6d4
Aruna Herath <aruna@kadira.iogit config --global user.name Aruna>
2015-09-15 18:17:31 +0530

    Add buffered writes

--- conn.go
+++ conn.go
@@ -1,16 +1,21 @@
 package fastcall
 
 import (
+	"bufio"
 	"encoding/binary"
+	"errors"
+	"io"
 	"net"
 	"sync"
 )
 
 // Conn sends and receives binary messages over tcp
 type Conn struct {
-	conn net.Conn
-	rmtx sync.Mutex
-	wmtx sync.Mutex
+	conn   net.Conn
+	reader io.Reader
+	writer io.Writer
+	rmtx   sync.Mutex
+	wmtx   sync.Mutex
 }
 
 // Dial creates a connection to given address
@@ -20,7 +25,23 @@ func Dial(addr string) (c *Conn, err error) {
 		return nil, err
 	}
 
-	c = &Conn{conn: conn}
+	c = &Conn{conn: conn, reader: conn, writer: conn}
+	return c, err
+}
+
+// DialBuf is similar to Dial except writes from the returned connection
+// are buffered
+func DialBuf(addr string) (c *Conn, err error) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	c = &Conn{
+		conn:   conn,
+		writer: bufio.NewWriter(conn),
+		reader: conn,
+	}
 	return c, err
 }
 
@@ -32,7 +53,7 @@ func (c *Conn) Read() (b []byte, err error) {
 	var sz int
 	var szu32 uint32
 
-	if err = binary.Read(c.conn, binary.LittleEndian, &szu32); err != nil {
+	if err = binary.Read(c.reader, binary.LittleEndian, &szu32); err != nil {
 		return nil, err
 	}
 
@@ -42,7 +63,7 @@ func (c *Conn) Read() (b []byte, err error) {
 	buffer := b[:sz]
 	toRead := buffer[:]
 	for len(toRead) > 0 {
-		n, err := c.conn.Read(toRead)
+		n, err := c.reader.Read(toRead)
 		if err != nil {
 			return nil, err
 		}
@@ -62,14 +83,14 @@ func (c *Conn) Write(b []byte) (err error) {
 	szu32 := uint32(sz)
 
 	buffer := b[:sz]
-	if err := binary.Write(c.conn, binary.LittleEndian, szu32); err != nil {
+	if err := binary.Write(c.writer, binary.LittleEndian, szu32); err != nil {
 		return err
 	}
 
 	toWrite := buffer[:]
 	for len(toWrite) > 0 {
-		n, err := c.conn.Write(toWrite)
-		if err != nil {
+		n, err := c.writer.Write(toWrite)
+		if (err != nil) && (err != io.ErrShortWrite) {
 			return err
 		}
 
@@ -79,6 +100,25 @@ func (c *Conn) Write(b []byte) (err error) {
 	return nil
 }
 
+func (c *Conn) FlushWriter() (err error) {
+	bufWriter, ok := c.writer.(*bufio.Writer)
+	if ok {
+		c.wmtx.Lock()
+		defer c.wmtx.Unlock()
+		return flushWriter(bufWriter)
+	}
+
+	return errors.New("not buffered")
+}
+
+func flushWriter(writer *bufio.Writer) (err error) {
+	err = writer.Flush()
+	for err == io.ErrShortWrite {
+		err = writer.Flush()
+	}
+	return err
+}
+
 // Close closes the connection
 func (c *Conn) Close() (err error) {
 	c.rmtx.Lock()
@@ -86,6 +126,11 @@ func (c *Conn) Close() (err error) {
 
 	c.wmtx.Lock()
 	defer c.wmtx.Unlock()
+
+	bufWriter, ok := c.writer.(*bufio.Writer)
+	if ok {
+		flushWriter(bufWriter)
+	}
 
 	if err := c.conn.Close(); err != nil {
 		return err
